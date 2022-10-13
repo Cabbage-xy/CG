54f1c93570720868434b1d08d29cc3adead1e2dc
sherlockcai <sherlockcai@tencent.com>
2022-10-13 18:04:38 +0800

    feat: support scan-line fill

---
+++ CG2019112315_cxy/CGRenderContext.cpp
@@ -0,0 +1,277 @@
+#include "pch.h"
+#include "CGRenderContext.h"
+#include "CGView.h"
+
+CG_NAMESPACE_ENTER
+CGRenderContext::CGRenderContext()
+{
+	mView = nullptr;
+	mHWND = 0;
+	mHDC = 0;
+	mHGLRC = 0;
+	mWidth = 0;
+	mHeight = 0;
+}
+CGRenderContext::CGRenderContext(CGView* pView)
+{
+	mView = pView;
+	if (pView != nullptr)
+	{
+		mHWND = pView->GetSafeHwnd();
+		mHDC = ::GetDC(mHWND);
+		CRect rc;
+		pView->GetClientRect(&rc);
+		mWidth = rc.Width();
+		mHeight = rc.Height();
+		mHGLRC = 0; //mHGLRC = ::wglCreateContext(mHDC); //三维派生类中初始化
+	}
+	else
+	{
+		mHWND = 0;
+		mHDC = 0;
+		mHGLRC = 0; //
+		mWidth = 0;
+		mHeight = 0;
+	}
+}
+CGRenderContext::~CGRenderContext()
+{
+	if (mHDC != 0)
+	{
+		::ReleaseDC(mHWND, mHDC); //释放DC
+		mHDC = 0;
+	}
+}
+void CGRenderContext::setView(CGView* pView)
+{
+	mView = pView;
+	if (mView != nullptr)
+	{
+		mHWND = mView->GetSafeHwnd();
+			if (mHDC != 0)
+			{
+				::ReleaseDC(mHWND, mHDC); //释放DC
+				mHDC = 0;
+			}
+		mHDC = ::GetDC(mHWND);//二维派生类中设置
+		CRect rc;
+		pView->GetClientRect(&rc);
+		mWidth = rc.Width();
+		mHeight = rc.Height();
+		//mHGLRC = ::wglCreateContext(mHDC); //三维派生类中设置
+	}
+}
+CGView* CGRenderContext::getView() const
+{
+	return mView;
+}
+void CGRenderContext::DDALine(const Vec2i& start, const Vec2i& end, unsigned long color)
+{
+	if (mView == nullptr && mHWND == 0 && mHDC == 0)
+		return;
+#ifdef USEMEMDC
+	if (mMemDC == 0)
+		return;
+#endif
+	HDC hDC = 0;
+#ifdef USEMEMDC
+	hDC = hmemDC(); //使用双缓存（内存DC）
+#else
+	hDC = hdc(); //不使用双缓存（内存DC）
+#endif
+	CClientDC dc(mView); //如果hDC为0时使用
+	//以下是直线段的绘制（自行补充）
+	int x0 = start.x(), y0 = start.y();
+	int x1 = end.x(), y1 = end.y();
+	int dx = x1 - x0, dy = y1 - y0;
+	float x, y, xIncre, yIncre;
+	int epsl;
+	x = x0; y = y0;
+	if (abs(dx) < abs(dy)) epsl = abs(dy);
+	else  epsl = abs(dx);
+	xIncre = (float)dx / (float)epsl;
+	yIncre = (float)dy / (float)epsl;
+
+	if (hDC != 0)
+	{
+		for (int i = 0; i <= epsl; i++) {
+			::SetPixel(hDC, (int)(x + 0.5), (int)(y + 0.5), color);
+			x += xIncre;
+			y += yIncre;
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= epsl; i++) {
+			dc.SetPixel((int)(x + 0.5), (int)(y + 0.5), color);
+			x += xIncre;
+			y += yIncre;
+		}
+	}
+	return;
+}
+void CGRenderContext::DDALine(const Vec2i& start, const Vec2i& end, unsigned long color, int width)
+{
+	return;
+}
+void CGRenderContext::DDALine(const Vec2i& start, const Vec2i& end, unsigned long color, int width, int style)
+{
+	return;
+}
+void CGRenderContext::MidPointLine(const Vec2i& start, const Vec2i& end, unsigned long color)
+{
+	return;
+}
+void CGRenderContext::MidPointLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width)
+{
+	return;
+}
+void CGRenderContext::MidPointLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width, int style)
+{
+	return;
+}
+void CGRenderContext::BresenhamLine(const Vec2i& start, const Vec2i& end, unsigned long color)
+{
+	return;
+}
+void CGRenderContext::BresenhamLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width)
+{
+	return;
+}
+void CGRenderContext::BresenhamLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width, int style)
+{
+	return;
+}
+void CGRenderContext::MidPointCircle(const Vec2i& center, int radius, unsigned long color) {
+	return;
+}
+void CGRenderContext::BresenhamCircle(const Vec2i& center, int radius, unsigned long color) {
+	return;
+}
+
+int Compare(const void *a, const void *b) {
+	float *pa = (float*)a;
+	float *pb = (float*)b;
+	return (*pa) - (*pb);  //从小到大排序
+}
+
+void CGRenderContext::ScanLinePolygonFill(const Vec2iArray& pnts, unsigned long fillcolor)
+{
+	// table = int[y][] 
+	// lines(ptns)
+	// loop (var line in lines){ for (y:low->high){ table[y] << hitX; } }
+
+	// for (y: low -> high) { table[y].sort; for (x) { if odd? drawP() } }
+
+
+	
+	int yLow, yHigh, xLow, xHigh;
+	CalculateBounds(pnts, yLow, yHigh, xLow, xHigh);
+	
+	std::unordered_map<int, std::vector<float>> yHitsMap;
+	PrepareMap(yHitsMap, pnts, yHigh, yLow);
+
+	
+	for (int y = yLow; y <= yHigh; y++) {
+		//std::qsort(yHitsMap.at(y).data(), yHitsMap.at(y).size(), sizeof(std::vector<float>), Compare);
+		sort(yHitsMap.at(y).begin(), yHitsMap.at(y).end());
+			
+		int counter = 0;
+		for (int x = xLow; x <= xHigh; x++) {
+			if (x >= yHitsMap.at(y)[counter])
+			{
+				DrawPixel(x, y, fillcolor);
+				counter++;
+			}
+
+			if (IsOdd(counter))
+				DrawPixel(x, y, fillcolor);
+		}
+	}
+	
+
+}
+
+void CGRenderContext::DrawPixel(int x, int y, unsigned long fillcolor) {
+	if (mView == nullptr && mHWND == 0 && mHDC == 0)
+		return;
+#ifdef USEMEMDC
+	if (mMemDC == 0)
+		return;
+#endif
+	HDC hDC = 0;
+#ifdef USEMEMDC
+	hDC = hmemDC(); //使用双缓存（内存DC）
+#else
+	hDC = hdc(); //不使用双缓存（内存DC）
+#endif
+	CClientDC dc(mView); //如果hDC为0时使用
+	//以下是直线段的绘制（自行补充）
+	if (hDC != 0)
+	{
+		::SetPixel(hDC, (int)(x + 0.5), (int)(y + 0.5), fillcolor);
+	}
+	else
+	{
+		dc.SetPixel((int)(x + 0.5), (int)(y + 0.5), fillcolor);
+	}
+}
+
+bool CGRenderContext::IsOdd(int num) {
+	return num % 2;
+}
+
+void  CGRenderContext::CalculateBounds(const Vec2iArray& pnts, int& yLow, int& yHigh,
+	int& xLow, int& xHigh) {
+	yLow = xLow = 25000;
+	for (auto pnt : pnts) {
+		if (pnt.x() > xHigh) xHigh = pnt.x();
+		if (pnt.y() > yHigh) yHigh = pnt.y();
+		if (pnt.x() < xLow) xLow = pnt.x();
+		if (pnt.x() < yLow) yLow = pnt.y();
+	}
+}
+
+void CGRenderContext::PrepareMap(std::unordered_map<int, std::vector<float>>& yHitsMap, const Vec2iArray &pnts, int yHigh, int yLow) {
+	//auto slopes = GetSlopes();
+
+	//yHitsMap = std::unordered_map<int, std::vector<float>>;
+
+	for (int p1 = 0; p1 < pnts.size(); p1++) {
+		int p2 = (p1 + 1) % pnts.size();
+
+		GetHits(pnts[p1], pnts[p2], yLow, yHigh, yHitsMap);
+	}
+}
+
+void CGRenderContext::GetHits(Vec2i p1, Vec2i p2, int yLow, int yHigh, std::unordered_map<int, std::vector<float>>& yHitsMap) {
+	//std::vector<float> slopes(pnts.size());
+	//for (int i = 0; i < pnts.size() - 1; i++) {
+	//std::vector<float> hits;
+	if(p2.y() == p1.y()) return;
+	float k =  (p2.x() - p1.x()) / (p2.y() - p1.y());
+
+	for (int y = yLow; y <= yHigh; y++) {
+		float hit = k * (y - p1.y()) + p1.x();
+		bool found = yHitsMap.find(y) != yHitsMap.end();
+		if (!found) {
+			yHitsMap.insert({ y, std::vector<float>() });
+		}
+
+		yHitsMap[y].push_back(hit);
+
+		//std::vector<float> hits;/*
+		/*hits.pop_back(hit)*//*;
+		yHitsMap.insert(y, hits);*/
+
+		//yHitsMap[y].push_back(hit);
+		//hits.push_back(hit);
+	}
+	//return hits;
+
+	//}
+	//slopes.back() = (pnts.front().x() - pnts.back().x()) / (pnts.front().y() - pnts.back().y());
+	//return slopes;
+}
+
+CG_NAMESPACE_EXIT
---
+++ CG2019112315_cxy/CGRenderContext.h
@@ -0,0 +1,95 @@
+#ifndef _CGRENDERCONTEXT_H_INCLUDED
+#define _CGRENDERCONTEXT_H_INCLUDED
+#include "Vector2.h"
+#include "CGObject.h"
+#include <unordered_map>
+#include <algorithm>
+
+CG_NAMESPACE_ENTER
+class CGView;//实验中视图类的基类，如果类的声明中只需要对象指针，则考虑类预声明。
+class CGRenderContext : public CGObject
+{
+public:
+	CGRenderContext();
+	CGRenderContext(CGView* pView);
+	virtual ~CGRenderContext();
+	virtual void setView(CGView* pView);
+	CGView* getView() const;
+	//辅助函数
+	HWND hwnd() const { return mHWND; }
+	HDC hdc() const { return mHDC; }
+	HGLRC hglrc() const { return mHGLRC; }
+	int width() const { return mWidth; }
+	int height() const { return mHeight; }
+public:
+	//算法定义
+	//直线绘制算法
+	static const int aLineCDC = 0; //使用CDC类的直线绘制函数
+	static const int aLineDDA = 1; //DDA画线算法
+	static const int aLineMidPoint = 2; //中点画线算法
+	static const int aLineBresenham = 3; //Bresenham画线算法
+	//圆绘制算法
+	static const int aCircleCDC = 0; //使用CDC类的画圆函数
+	static const int aCrcleMidPoint = 1; //中点画圆算法
+	static const int aCircleBresenham = 2; //Bresenham画圆算法
+	static const int aCirclePolygon = 3; //多边形逼近画圆
+	//椭圆绘制算法
+	static const int aEllipseCDC = 0; //使用CDC类的画椭圆函数
+	static const int aEllipseMidPoint = 1; //中点画椭圆算法
+	//多边形填充算法
+	static const int aFillCDC = 0; //使用CDC类的填充函数
+	static const int aFillScanlinePolygon = 1; //扫描线多边形填充算法
+	//点阵区域填充
+	static const int aFillSeedCDC = 0; //使用CDC类的填充函数
+	static const int aFillSeedBound4 = 1; //4联通边界种子填充算法
+	static const int aFillSeedBound8 = 2; //8联通边界种子填充算法
+	static const int aFillSeedFlood4 = 3; //4联通内点种子填充算法
+	static const int aFillSeedFlood8 = 4; //8联通内点种子填充算法
+	static const int aFillSeedScanline = 5; //扫描线种子填充算法
+public:
+	//直线段绘制算法
+	virtual void DDALine(const Vec2i& start, const Vec2i& end, unsigned long color);
+	virtual void DDALine(const Vec2i& start, const Vec2i& end, unsigned long color, int width);
+	virtual void DDALine(const Vec2i& start, const Vec2i& end, unsigned long color, int width, int style);
+	virtual void MidPointLine(const Vec2i& start, const Vec2i& end, unsigned long color);
+	virtual void MidPointLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width);
+	virtual void MidPointLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width, int style);
+	virtual void BresenhamLine(const Vec2i& start, const Vec2i& end, unsigned long color);
+	virtual void BresenhamLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width);
+	virtual void BresenhamLine(const Vec2i& start, const Vec2i& end, unsigned long color, int width, int style);
+	
+	//圆绘制算法
+	virtual void MidPointCircle(const Vec2i& center, int radius, unsigned long color);
+	virtual void BresenhamCircle(const Vec2i& center, int radius, unsigned long color);
+	
+	// virtual void CircleArc(const Vec2i& center, int radius, unsigned long color, float startAngle, float sweepAngle); //逆时针
+	
+	//椭圆绘制算法（轴对齐椭圆）
+	//virtual void MidPointEllipse(const Vec2i& center, int a, int b, unsigned long color);
+	//virtual void EllipseArc(const Vec2i& center, int a, int b, unsigned long color, float startAngle, float sweepAngle); //逆时针
+	//有效边表多边形扫描线填充算法
+	virtual void ScanLinePolygonFill(const Vec2iArray& pnts, unsigned long fillcolor);
+	void DrawPixel(int x, int y, unsigned long fillcolor);
+	bool IsOdd(int num);
+	void CalculateBounds(const Vec2iArray & pnts, int & yLow, int & yHigh, int & xLow, int & xHigh);
+	void PrepareMap(std::unordered_map<int, std::vector<float>>& yHitsMap, const Vec2iArray & pnts, int yHigh, int yLow);
+	void GetHits(Vec2i p1, Vec2i p2, int yLow, int yHigh, std::unordered_map<int, std::vector<float>>& yHitsMap);
+	//int Compare(const void * a, const void * b);
+	//边界表示的种子填充算法
+	//virtual void BoundFill4(int x, int y, unsigned long boundcolor, unsigned long fillcolor);
+	//virtual void BoundFill8(int x, int y, unsigned long boundcolor, unsigned long fillcolor);
+	//内点表示的种子填充算法
+	//virtual void FloodFill4(int x, int y, unsigned long innercolor, unsigned long fillcolor);
+	//virtual void FloodFill8(int x, int y, unsigned long innercolor, unsigned long fillcolor);
+	//扫描线种子填充算法
+	//virtual void ScanLineSeedFill(int x, int y, unsigned long boundcolor, unsigned long fillcolor);
+	//virtual void ScanLinePolygonFill(const Vec2iArray& pnts, unsigned long fillcolor);
+protected:
+	CGView* mView; //通过视图类可以访问程序主框架中的一些设置，比如当前线型、线宽等。
+	HWND mHWND; //通过mView设置
+	HDC mHDC; //通过mView设置
+	HGLRC mHGLRC;//通过mView设置，三维图形用
+	int mWidth, mHeight; //视图客户区（绘图窗口的宽、高）
+};
+CG_NAMESPACE_EXIT
+#endif //_CGRENDERCONTEXT_H_INCLUDED
