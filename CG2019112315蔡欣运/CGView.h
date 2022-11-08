f00b046bbf658ac34c274dca6d54c035d15da268
Cabbage-xy <75256787+Cabbage-xy@users.noreply.github.com>
2022-11-08 16:32:40 +0800

    Revert "Dev/cxy: 2d graph observe (#4)" (#5)
    This reverts commit a43910f7b24d913a5f5f55476818f09ba9bef7bb.

--- CG2019112315蔡欣运/CGView.h
+++ CG2019112315蔡欣运/CGView.h
@@ -27,16 +27,6 @@ public:
 	virtual bool UseBrush() const;
 	virtual COLORREF BrushColor() const;
 	virtual int BrushIndex() const;
-	//观察窗口宽度与视口宽度的比例
-	double WidthFactorWindowtoViewPort();
-	//观察窗口高度与视口高度的比例
-	double HeightFactorWindowtoViewPort();
-	//观察（二维）（注意在实现中要保持与视口高宽比一致）
-	virtual void Move2DCamera(double tx, double ty); //平移相机（也平移观察窗口）
-	virtual void Zoom2DCamera(const Vec2d& lb, const Vec2d& rt); //观察窗口左下角、右上角
-	virtual void Zoom2DCamera(double ratio); //给定观察窗口的缩放比例（）
-	virtual void Rotate2DCamera(double degree); //转动相机（观察坐标系）
-	virtual void Reset2DCamera(); //重置到默认参数（二维）
 	//画线算法
 	virtual int LineAlgorithm() const;
 	//圆弧算法
